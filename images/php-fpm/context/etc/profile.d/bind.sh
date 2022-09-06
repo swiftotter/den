2979ec9362f381e954c707dd4ed893e3313bab01
Navarr <navarr@swiftotter.com>
2022-09-06 08:43:12 -0400

    Update shell check not to use exit
    Fixes #61

--- images/php-fpm/context/etc/profile.d/bind.sh
+++ images/php-fpm/context/etc/profile.d/bind.sh
@@ -1,9 +1,5 @@
-if [ "$SHELL" != "/bin/bash" ]; then
-  exit 0
-fi
-
 # Setup history search ability (only bind when a tty is present)
-if [ -t 1 ]; then
+if [[ "$SHELL" == "/bi/bash" && -t 1 ]]; then
     bind '"\e[A":history-search-backward'
     bind '"\e[B":history-search-forward'
 fi
