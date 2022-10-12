4bec810236c7a841549f23fabe73964a31b05a3b
Brett Patterson <brett.patterson@classyllama.com>
2022-10-12 19:43:57 -0400

    Remove python dependency

--- commands/status.cmd
+++ commands/status.cmd
@@ -20,19 +20,23 @@ if [[ -z "${denNetworkId}" ]]; then
 fi
 
 denTraefikId=$(docker container ls --filter network="${denNetworkId}" --filter status=running --filter name=traefik -q)
+projectNetworkList=$(docker container inspect --format '{{ range $k,$v := .NetworkSettings.Networks }}{{ if ne $k "warden" }}{{printf "%s\n" $k }}{{ end }}{{end}}' "${denTraefikId}")
 
-denTraefikNetworks=$(docker container inspect "${denTraefikId}")
-projectList=$(echo "$denTraefikNetworks" | \
-    python3 -c "import json, sys; [print(''.join(name.rsplit('_default',1))) for name in sorted(json.load(sys.stdin)[0]['NetworkSettings']['Networks'].keys()) if name != '${denNetworkName}' ]")
-
-# echo ""
 echo -e "Found the following \033[32mrunning\033[0m environments:"
-for project in $projectList; do
-    projectContainer=$(docker network inspect "${project}_default" | \
-        python3 -c "import sys, json; print([container['Name'] for container in json.load(sys.stdin)[0]['Containers'].values() if container['Name'].startswith('${project}-')][0])")
-    projectDir=$(docker container inspect --format '{{ index .Config.Labels "com.docker.compose.project.working_dir"}}' "$projectContainer")
-
+for projectNetwork in $projectNetworkList; do
+    prefix="${projectNetwork%_default}"
+    prefixLen="${#prefix}"
+    ((prefixLen+=1))
+    projectContainers=$(docker network inspect --format '{{ range $k,$v := .Containers }}{{ $prefix := slice $v.Name 0 '"${prefixLen}"' }}{{ if eq $prefix "'"${prefix}-"'" }}{{ println $v.Name }}{{end}}{{end}}' "${projectNetwork}")
+    container=$(echo "$projectContainers" | head -n1)
+    
+    projectDir=$(docker container inspect --format '{{ index .Config.Labels "com.docker.compose.project.working_dir"}}' "$container")
+    projectName=$(cat "${projectDir}/.env" | grep '^WARDEN_ENV_NAME=' | sed -e 's/WARDEN_ENV_NAME=[[:space:]]*//g' | tr -d -)
     projectType=$(cat "${projectDir}/.env" | grep '^WARDEN_ENV_TYPE=' | sed -e 's/WARDEN_ENV_TYPE=[[:space:]]*//g' | tr -d -)
+    traefikDomain=$(cat "${projectDir}/.env" | grep '^TRAEFIK_DOMAIN=' | sed -e 's/TRAEFIK_DOMAIN=[[:space:]]*//g' | tr -d -)
 
-    echo -e "    - \033[1;35m${project}\033[0m a \033[36m${projectType}\033[0m project; located at \033[33m${projectDir}\033[0m"
+    echo -e "    \033[1;35m${projectName}\033[0m a \033[36m${projectType}\033[0m project"
+    echo -e "       Project Directory: \033[33m${projectDir}\033[0m"
+    echo -e "       Project URL: \033[94mhttps://${traefikDomain}\033[0m"
+    echo "   "
 done
