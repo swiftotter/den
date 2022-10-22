2666df5db663543121d317ad4a9dea9562096345
Brett Patterson <brett.patterson@classyllama.com>
2022-10-21 23:11:05 -0400

    Adding Fish and Zsh alternative shells

--- commands/shell.cmd
+++ commands/shell.cmd
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 [[ ! ${WARDEN_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
+# Load in the user-specific shell command
+if [[ -f "${WARDEN_HOME_DIR}/.env" ]]; then
+    eval "$(cat "${WARDEN_HOME_DIR}/.env" | sed 's/\r$//g' | grep "^WARDEN_ENV_SHELL_COMMAND")"
+fi
+
 WARDEN_ENV_PATH="$(locateEnvPath)" || exit $?
 loadEnvConfig "${WARDEN_ENV_PATH}" || exit $?
 
 ## set defaults for this command which can be overridden either using exports in the user
 ## profile or setting them in the .env configuration on a per-project basis
-WARDEN_ENV_SHELL_COMMAND=${WARDEN_ENV_SHELL_COMMAND:-bash}
+WARDEN_ENV_SHELL_COMMAND=${USE_SHELL:-${WARDEN_ENV_SHELL_COMMAND:-bash}}
 WARDEN_ENV_SHELL_CONTAINER=${WARDEN_ENV_SHELL_CONTAINER:-php-fpm}
 
 ## allow return codes from sub-process to bubble up normally
